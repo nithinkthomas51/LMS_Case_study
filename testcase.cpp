0527e40e4793f2fe676042696df057f485c38f54
nithinkthomas51 <64864340+nithinkthomas51@users.noreply.github.com>
2020-11-24 10:14:51 +0530

    Update testcase.cpp

--- testcase.cpp
+++ testcase.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(void){
-	cout<<"Hello world\n";
+    cout<<"Hello world\n";
     cout<<" Mariya\n";
-	  cout<<"Hello world\n";
+    cout<<"Hello world\n";
     cout<<"I am Roshin"<<endl;
     cout<<"Sanjay \n
     cout<<"Shahma \n";
     cout<<"changed \n";
     cout<<"Piller Kollam\n";
-	return 0;
+    return 0;
 	}
 
